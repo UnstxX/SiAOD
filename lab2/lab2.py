19e7a1d8d611b5e0dcbeb16092650872026db956
UnstxX <vladislav.platonoff@gmail.com>
2022-03-04 09:09:25 +0300

    Lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,59 +1,112 @@
-board = [[0 for i in range(8)] for j in range(8)]
-counter = 0
-operation_counter = 0
+import copy
+from random import randint
+import time
 
 
-def set_queen(i, j):
-    global operation_counter
-    operation_counter += 1
-    for x in range(8):
-        board[x][j] += 1
-        board[i][x] += 1
-        if 0 <= i + j - x < 8:
-            board[i + j - x][x] += 1
-        if 0 <= i - j + x < 8:
-            board[i - j + x][x] += 1
-    board[i][j] = -1
+def list_create():
+    length = int(input("Введите длину массива: "))
+    list_of_items = [randint(-500,500) for j in range(length)]
+    list_of_items.sort()
+    print(list_of_items)
+    return list_of_items
 
 
-def remove_queen(i, j):
-    for x in range(8):
-        board[x][j] -= 1
-        board[i][x] -= 1
-        if 0 <= i + j - x < 8:
-            board[i + j - x][x] -= 1
-        if 0 <= i - j + x < 8:
-            board[i - j + x][x] -= 1
-    board[i][j] = 0
+def delete_element(list_of_items):
+    index = int(input("Введите индекс числа, которое хотите удалить: "))
+    del list_of_items[index]
+    print(list_of_items)
+    return list_of_items
 
 
-def print_position():
-    global counter
-    counter += 1
-    alphabet = 'abcdefgh'
-    answer = []
-    for i in range(8):
-        for j in range(8):
-            if board[i][j] == -1:
-                answer.append(alphabet[j] + str(i + 1))
-    print(','.join(answer))
+def insert_element(list_of_items):
+    index = int(input("Введите индекс числа, на который хотите вставить число: "))
+    number = int(input("Введите число, которое хотите вставить: "))
+    list_of_items.insert(index, number)
+    print(list_of_items)
+    return list_of_items
 
 
-def solve(i):
-    for j in range(8):
-        if board[i][j] == 0:
-            set_queen(i, j)
-            if i == 7:
-                print_position()
+def binary_search(list_of_numbers, value):
+    first = 0
+    last = len(list_of_numbers) - 1
+    index = -1
+
+    while first <= last and index == -1:
+        mid = (first + last) // 2
+        if list_of_numbers[mid] == value:
+            index = mid
+        else:
+            if value < list_of_numbers[mid]:
+                last = mid - 1
             else:
-                solve(i + 1)
-            remove_queen(i, j)
+                first = mid + 1
+    return index
+
+
+def fibonacci_search(list_of_items, value):
+    fib_minus_2 = 0
+    fib_minus_1 = 1
+    fib = fib_minus_1 + fib_minus_2
+    while fib < len(list_of_items):
+        fib_minus_2 = fib_minus_1
+        fib_minus_1 = fib
+        fib = fib_minus_1 + fib_minus_2
+    index = -1
+    while fib > 1:
+        i = min(index + fib_minus_2, (len(list_of_items) - 1))
+        if list_of_items[i] < value:
+            fib = fib_minus_1
+            fib_minus_1 = fib_minus_2
+            fib_minus_2 = fib - fib_minus_1
+            index = i
+        elif list_of_items[i] > value:
+            fib = fib_minus_2
+            fib_minus_1 = fib_minus_1 - fib_minus_2
+            fib_minus_2 = fib - fib_minus_1
+        else:
+            return i
+    if fib_minus_1 and index < len(list_of_items) - 1 and list_of_items[index + 1] == value:
+        return index + 1
+    return -1
+
+
+def interpolation_search(list_of_items, value):
+    low = 0
+    high = len(list_of_items) - 1
+    while low <= high and value >= list_of_items[low] and value <= list_of_items[high]:
+        index = low + int(((float(high - low) / (list_of_items[high] - list_of_items[low])) * (value - list_of_items[low])))
+        if list_of_items[index] == value:
+            return index
+        if list_of_items[index] < value:
+            low = index + 1
+        else:
+            high = index - 1
+    return -1
 
 
 def main():
-    solve(0)                   # Вывод всех вариантов решения задачи
-    print(counter)             # Вывод числа решений
-    print(operation_counter)   # Вывод числа проведенных операций
+    print("Исходный список:")
+    list_of_items = list_create()
+    list_of_items_binary = copy.deepcopy(list_of_items)
+    print("Бинарный поиск: ")
+    value = int(input("Введите элемент для поиска: "))
+    print("Искомый элемент имеет индекс " + binary_search(list_of_items_binary, value))
+    insert_element(list_of_items_binary)
+    delete_element(list_of_items_binary)
+
+    list_of_items_fibonacci = copy.deepcopy(list_of_items)
+    print("Фибоначчиев поиск: ")
+    value = int(input("Введите элемент для поиска: "))
+    print("Искомый элемент имеет индекс " + fibonacci_search(list_of_items_fibonacci, value))
+    insert_element(list_of_items_fibonacci)
+    delete_element(list_of_items_fibonacci)
+
+    list_of_items_interpolation = copy.deepcopy(list_of_items)
+    print("Интерполяционный поиск: ")
+    value = int(input("Введите элемент для поиска: "))
+    print("Искомый элемент имеет индекс " + interpolation_search(list_of_items_interpolation, value))
+    insert_element(list_of_items_interpolation)
+    delete_element(list_of_items_interpolation)
 
 
 if __name__ == "__main__":
